90d87a5e7e26409d15fcd678a53969790ed04bf9
aliqa-stack <alikalmaliki51@gmail.com>
2026-09-14 17:59:57 +0700

    second model

--- nn.py
+++ nn.py
@@ -1,20 +1,71 @@
 import os
 import torch
 from torch import nn
+import torch.nn.functional as f
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torchvision.transforms import v2
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
 
-class Network(nn.Modules):
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
-        self.layer_relu = nn.Sequentian(
-            nn.linear(28*28, 512),
+        self.flatten = nn.Flatten()
+        self.layer_relu = nn.Sequential(
+            nn.Linear(28*28, 512),
             nn.ReLU(),
-            nn.linear(512, 512),
-            nn.ReLU()
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
         )
     def forward(self, x):
+        x = self.flatten(x)
         logic = self.layer_relu(x)
         return logic
 
 model = Network()
+batch_size = 64
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        err = loss_fn(pred, y)
+        err.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if batch % 100 == 0:
+            err, current = err.item(), batch * batch_size + len(X)
+            print(f"loss : {err:>7f}  current: {current:>5d} / {size:>5d}")
+
+train_loop(train_dataloader, model, loss_fn, optimizer)
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y)
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+test_loop(train_dataloader, model, loss_fn)
